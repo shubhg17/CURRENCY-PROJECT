187f9e0dcf19c59fb7418f6794899e254d532cf0
shubhg17 <shubamghai116@gmail.com>
2025-11-30 19:03:24 +0530

    Created custom hook and added API

--- src/App.jsx
+++ src/App.jsx
@@ -11,3 +11,5 @@ function App() {
 }
 
 export default App
+
+//Hook in the end ek function hi ha toh tum custom hooks banaskte ho aur yeh file name hum JSX nhi rkhenge kyuki yeh hooks majority of the cases me js return krte ha aur jabh js return ho rha ho toh js rkho jsx return ho toh jsx rakho file name yeh ek achi practice hoti ha 
