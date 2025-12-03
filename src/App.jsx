035ba9f3402a54376f508e4a2c9ab89f10364d97
shubhg17 <shubamghai116@gmail.com>
2025-12-04 02:20:01 +0530

    Made some changes for exporting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
+import Inputbox from './components/Inputbox'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1 className="heading">Currency App with Chai</h1>
+      {/* <h1 className="heading">Currency App with Chai</h1> */}
+      <Inputbox/>
     </>
   )
 }
